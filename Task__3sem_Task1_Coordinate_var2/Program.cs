1b1a3ba361e28c7567750a696a88c18817a83cdc
Dashch <dashch88@gmail.com>
2022-11-25 14:28:03 +0400

    Added Seminar 3. Task 1. Variant 2. Coordinate quarter. Switch

---
+++ Task__3sem_Task1_Coordinate_var2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1.
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// 1 -> x > 0, y > 0
+// 3 -> x < 0, y < 0
+
+// через switch
+
+Console.Clear();
+
+Console.WriteLine("Введите номер четверти координатной плоскости.");
+int num = int.Parse(Console.ReadLine());
+switch (num)
+{
+    case 1:
+        Console.WriteLine("x > 0, y > 0");
+        break;
+    case 2:
+        Console.WriteLine("x < 0, y > 0");
+        break;
+    case 3:
+        Console.WriteLine("x < 0, y < 0");
+        break;
+    case 4:
+        Console.WriteLine("x > 0, y < 0");
+        break;
+
+    default:
+        Console.WriteLine("Вы ввели что-то другое!");
+        break;
+}
