a8a3eda66a057b5641d079f1b42095c3876f1463
Dashch <dashch88@gmail.com>
2022-11-25 14:53:15 +0400

    Added Seminar 3. Task 3. Table of squares of numbers

---
+++ Task__3sem_Task3_TableSqrNum/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+
+// 2 -> 1,4
+
+Console.Clear();
+
+Console.WriteLine("Введите N. Целое, положительное.");
+int num = int.Parse(Console.ReadLine());
+double sqr = 1;
+
+Console.WriteLine();
+
+if (num < 1)
+{
+    Console.WriteLine("Вы ввели число не удовлетворяющее условиям.");
+}
+else
+{
+    for (int i = 1; i < num + 1; i++)
+    {
+        sqr = Math.Pow(i, 2);
+        Console.Write(sqr + " ");
+    }
+}
+Console.WriteLine();
