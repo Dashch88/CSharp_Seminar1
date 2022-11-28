8ee37e9c86dd3a4925e1f39970cd8c4840435bed
Dashch <dashch88@gmail.com>
2022-11-28 19:42:20 +0400

    Added Seminar 4. Task 2. Product of Numbers

---
+++ Task__4sem_Task2_ProdOfN/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2:
+// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4  -> 24
+// 5  -> 120
+
+int ProdOfN(int n)
+{
+    int prod = 1;
+    for (int i = 2; i < n + 1; i++)
+    {
+        prod = prod * i;
+    }
+    return prod;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число N для которого нужно найти произвдение чисел от 1 до N.");
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Произведение чисел от 1 до введённого N: " + ProdOfN(num));
