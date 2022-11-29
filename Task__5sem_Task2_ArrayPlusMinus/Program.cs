87dd5d7be02c898aa00f08182db89ab69d6529ee
Dashch <dashch88@gmail.com>
2022-11-29 17:52:52 +0400

    Added Seminar 5. Task 1. Random Array Plus on Minus.

---
+++ Task__5sem_Task2_ArrayPlusMinus/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Задайте массив из 10 элементов и положительные элементы замените 
+// на соответствующие отрицательные, и наоборот.
+
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+Console.Clear();
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= -1;
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
